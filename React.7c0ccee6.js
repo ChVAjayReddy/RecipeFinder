656a9f17a5cd45984be9e550b8a48eed5baf10fd
Ch V Ajay Reddy <ajayreddy1997@gmail.com>
2025-09-03 14:33:31 +0530

    Updates

---
+++ React.7c0ccee6.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("h1", {
+    id: "ajay",
+    gender: "male"
+}, "Hello world from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=React.7c0ccee6.js.map
